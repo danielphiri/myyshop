057338be54efa620b8cafa23a06b78fd53eadf24
danielphiri <danielphiri@berkeley.edu>
2023-06-19 12:52:46 -0400

    started landing page ui main content area implementation

--- MyyShop/LandingPage.swift
+++ MyyShop/LandingPage.swift
@@ -9,8 +9,52 @@ import SwiftUI
 
 struct LandingPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      ScrollView(showsIndicators: true) {
+        LazyVStack {
+          ZStack {
+            Image("banner-image")
+              .resizable()
+              .aspectRatio(contentMode: .fill)
+              .background(Color.red)
+            VStack(alignment: .center, spacing: 20) {
+              Text("MyyShop is Your Shop!")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .fixedSize(horizontal: false, vertical: true)
+              Text("Empowers content creators to monetize their social media presence!")
+                .font(.title3)
+                .fixedSize(horizontal: false, vertical: true)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+              signUpButton
+                .padding(.horizontal, 40)
+                .padding(.top, 50)
+            }
+            .frame(width: UIScreen.main.bounds.width)
+          }
+          .frame(height: UIScreen.main.bounds.height * 0.5)
+        }
+      }
+      .ignoresSafeArea()
     }
+  
+  
+  private var signUpButton: some View {
+    Button {
+      #warning("TODO")
+    } label: {
+      ZStack {
+        RoundedRectangle(cornerRadius: 0)
+          .foregroundColor(.white)
+        Text("Sign up as Creator")
+          .foregroundColor(.black)
+          .font(.callout)
+      }
+      
+    }
+    .frame(height: 60)
+  }
+  
 }
 
 struct LandingPage_Previews: PreviewProvider {
